ef5765c5d3132306d14b16aedbe451ea8a3b0348
Florian Guitton <f.guitton@imperial.ac.uk>
2019-04-05 21:28:12 +0100

    Project moved

--- config/borderline.test.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    mongoURL: 'mongodb://mongodb/borderline',
-    extensionSourcesFolder: '.plugins/sources',
-    extensionWatchFolder: true,
-    port: 3000,
-    development: false,
-};
